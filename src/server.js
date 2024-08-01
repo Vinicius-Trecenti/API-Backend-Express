165788734a46641981d0949cb9d2c303fe66a04d
Vinicius-Trecenti <vinitrecenti@gmail.com>
2024-07-31 22:17:27 -0300

    Add import type module and hello world express

--- src/server.js
+++ src/server.js
@@ -1,1 +1,16 @@
-console.log("Olá Mundo!")
+//const express = require('express')
+import express from 'express'
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/user', (req, res) => {
+    res.json({ nome: "Vinicius", email: "vinitrecenti@gmail.com"})
+})
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`)
+}) 
